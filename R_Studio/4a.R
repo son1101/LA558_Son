4707633eb5a83c8ee0a1b0624cb22eadecdb836f
Son <son1101@iastate.edu>
2023-02-28 23:38:57 -0600

    summary

---
+++ R_Studio/4a.R
@@ -0,0 +1,9 @@
+# Hyunsik Son
+# Lecture 4a
+# LA 558, Spring 2023
+
+#Packages and libraries ####
+install.packages("tidyverse")
+install.packages("tidycensus")
+library(tidyverse)
+library(tidycensus)
